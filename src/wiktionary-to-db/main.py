ae74828e3f0ad36808d7037d6c3938833fb26657
Ben Wilson <ben.wilson1337@gmail.com>
2022-08-13 03:38:33 -0400

    add venv, fix failing imports, fix failing output files

--- src/wiktionary-to-db/main.py
+++ src/wiktionary-to-db/main.py
@@ -4,35 +4,43 @@ import pprint
 import re
 from mediawiki import *
 
-from lib.db import deleteAllWordData, insertWordData
-from lib.outputfile import Subdir, initDir, slugify, writeFile, writeFileFromList
-from lib.timer import startTimer, stopTimer
-from .wiktionaryparser import doesTextSmellLikeValidPronunciation, getIpasFromWikiHtml
+from src.lib.db import deleteAllWordData, insertWordData
+from src.lib.outputfile import initDir, slugify, writeFile, writeFileFromList
+from src.lib.timer import startTimer, stopTimer
+from wiktionaryparser import doesTextSmellLikeValidPronunciation, getIpasFromWikiHtml
 
 
 def main():
     # ----- controls -----
     DUMP_PATH = "C:\\Users\\ben\\Downloads\\enwiktionary-20220701-pages-meta-current.xml"
-    SHOULD_CLEAR_NONSMELL_FILES = True
-    SHOULD_WRITE_NONSMELL_FILES = False
-    SHOULD_CLEAR_HTML_FILES = True
-    SHOULD_WRITE_HTML_FILES = False
+    REPORT_DIR = 'wiktionary-to-db/report'
+    SHOULD_CLEAR_REPORT_FILES = False
+    SHOULD_WRITE_REPORT_FILES = True
+    NONSMELL_PAGE_DIR = 'wiktionary-to-db/non-smell-pages'
+    SHOULD_CLEAR_NONSMELL_PAGES = True
+    SHOULD_WRITE_NONSMELL_PAGES = False
+    HTML_PAGE_DIR = 'wiktionary-to-db/html'
+    SHOULD_CLEAR_HTML_PAGES = True
+    SHOULD_WRITE_HTML_PAGES = False
+    FAILED_PAGE_DIR = 'wiktionary-to-db/failed-pages'
     SHOULD_CLEAR_FAILED_PAGES = False
     SHOULD_WRITE_FAILED_PAGES = True
     SHOULD_CLEAR_WORDS_IN_DB = False
     SHOULD_INSERT_WORDS_TO_DB = False
     NUM_PAGES_TO_SKIP = 0
-    NUM_PAGES_TO_PARSE = 1000
+    NUM_PAGES_TO_PARSE = 50
     # ----- end controls -----
 
     startTimer()
 
-    if SHOULD_WRITE_NONSMELL_FILES or SHOULD_CLEAR_NONSMELL_FILES:
-        initDir(Subdir['NONSMELL'])
-    if SHOULD_WRITE_HTML_FILES or SHOULD_CLEAR_HTML_FILES:
-        initDir(Subdir['HTML'])
+    if SHOULD_CLEAR_REPORT_FILES or SHOULD_WRITE_REPORT_FILES:
+        initDir(REPORT_DIR)
+    if SHOULD_WRITE_NONSMELL_PAGES or SHOULD_CLEAR_NONSMELL_PAGES:
+        initDir(NONSMELL_PAGE_DIR)
+    if SHOULD_WRITE_HTML_PAGES or SHOULD_CLEAR_HTML_PAGES:
+        initDir(HTML_PAGE_DIR)
     if SHOULD_WRITE_FAILED_PAGES or SHOULD_CLEAR_FAILED_PAGES:
-        initDir(Subdir['FAILED_PAGES'])
+        initDir(FAILED_PAGE_DIR)
 
     wikiDump = mwxml.Dump.from_file(open(DUMP_PATH, encoding='utf-8'))
 
@@ -54,14 +62,14 @@ def main():
         pageFileName = getPageFileName(pageIndex, page.id, page.title)
         if not text or len(text) == 0 or not doesTextSmellLikeValidPronunciation(text):
             pagesWithNoPronunSmell.append(pageFileName)
-            if SHOULD_WRITE_NONSMELL_FILES:
-                writeFile(Subdir['NONSMELL'], pageFileName + '.txt', text or '', shouldPrint=False)
+            if SHOULD_WRITE_NONSMELL_PAGES:
+                writeFile(NONSMELL_PAGE_DIR, pageFileName + '.txt', text or '', shouldPrint=False)
             continue
         text = removeNowikiTags(text)
         try:
             html = wiki2html(text, False)
-            if SHOULD_WRITE_HTML_FILES:
-                writeFile(Subdir['HTML'], pageFileName + '.html', html, shouldPrint=False)
+            if SHOULD_WRITE_HTML_PAGES:
+                writeFile(HTML_PAGE_DIR, pageFileName + '.html', html, shouldPrint=False)
             pageIpas = getIpasFromWikiHtml(html)
             if len(pageIpas) == 0:
                 pagesWithNoValidEnglishPronuns.append(pageFileName)
@@ -77,13 +85,13 @@ def main():
             errorStr = traceback.format_exc()
             print(errorStr)
             failedPages.append(f'{pageIndex}: {page.title}\n{errorStr}\n')
-            writeFile(Subdir['FAILED_PAGES'], pageFileName + '.txt', text or '')
+            writeFile(FAILED_PAGE_DIR, pageFileName + '.txt', text or '')
 
     print()
-    writeFileFromList(Subdir['REPORT'], 'processedWords.txt', [formatWordDataLine(word) for word in processedWords])
-    writeFileFromList(Subdir['REPORT'], 'pagesWithNoPronunSmell.txt', pagesWithNoPronunSmell)
-    writeFileFromList(Subdir['REPORT'], 'pagesWithNoValidEnglishPronuns.txt', pagesWithNoValidEnglishPronuns)
-    writeFileFromList(Subdir['REPORT'], 'failedPages.txt', failedPages)
+    writeFileFromList(REPORT_DIR, 'processedWords.txt', [formatWordDataLine(word) for word in processedWords])
+    writeFileFromList(REPORT_DIR, 'pagesWithNoPronunSmell.txt', pagesWithNoPronunSmell)
+    writeFileFromList(REPORT_DIR, 'pagesWithNoValidEnglishPronuns.txt', pagesWithNoValidEnglishPronuns)
+    writeFileFromList(REPORT_DIR, 'failedPages.txt', failedPages)
 
     if SHOULD_CLEAR_WORDS_IN_DB:
         deleteAllWordData()
